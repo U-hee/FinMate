51a08a0b2b9acb5c1be4df8dbae044573d07fddc
Logan <uhee0112@gmail.com>
2025-11-06 09:54:22 +0900

    First Commit

---
+++ src/main/java/com/life/finmate/FinMateBeApplication.java
@@ -0,0 +1,13 @@
+package com.life.finmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinMateBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinMateBeApplication.class, args);
+    }
+
+}
